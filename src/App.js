730a882f1a07cb066fd669fdd83d316e5f7b4259
HugoHern <hugo8954@gmail.com>
2022-08-09 20:49:19 -0400

    added routes functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import SideBar from "./components/SideBar/SideBar";
+import React from "react";
+import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<div>
+            <SideBar />
+            <RecommendedVideos />
+          </div>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RecommendedVideos() {
+  return (
+    <div>RecommendedVideos</div>
+  )
+}
