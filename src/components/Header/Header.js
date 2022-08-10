60cb56efd09242e9fcdcee41213c79a882a68f35
HugoHern <hugo8954@gmail.com>
2022-08-09 21:00:28 -0400

    imported material ui icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,13 @@
-import React from 'react'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/CircleNotifications';
+import Avatar from '@mui/icons-material/Face';
+import './Header.css';
 
 export default function Header() {
   return (
